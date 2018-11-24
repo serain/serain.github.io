b680371a131b3b810295fcbdf05619a0c21f0fcb
alxk <alxk@localhost>
2018-11-24 14:54:25 +0000

    redirect to https if http

--- js/min.js
+++ js/min.js
@@ -1,1 +1,11 @@
 console.log("%cL M A O\n%chttps://github.com/username","color:#666;font-weight:bold;font-size:21px;line-height:2;","color:#4078c0;font-size:14px;line-height:2;");
+
+var hosts = [
+  "serain.github.io",
+  "alex.kaskaso.li"
+];
+
+for (let host of hosts) {
+  if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
+}
